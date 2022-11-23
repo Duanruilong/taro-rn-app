3e52d79f78fc84211243e794e55a9562cb839466
Drlong <drl1210@163.com>
2022-11-23 16:54:11 +0800

    fix:APP_ID

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,16 @@
+/*
+ * @Author: duanruilong
+ * @Date: 2022-11-21 17:11:01
+ * @LastEditors: duanruilong
+ * @LastEditTime: 2022-11-21 17:55:59
+ * @Description:
+ */
 module.exports = {
   env: {
-    NODE_ENV: '"development"'
+    NODE_ENV: '"development"',
   },
-  defineConstants: {
-  },
+  defineConstants: {},
+  isWatch: true,
   mini: {},
-  h5: {}
-}
+  h5: {},
+};
